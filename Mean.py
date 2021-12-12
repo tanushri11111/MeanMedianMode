2d71098bbbcbc425e0c933dfa0e85c0f5dbca6dc
tanushri11111 <73070364+tanushri11111@users.noreply.github.com>
2021-12-12 13:04:12 +0530

    Add files via upload

---
+++ Mean.py
@@ -0,0 +1,30 @@
+import csv
+import pandas as pd
+import plotly.express as px
+with open("data.csv",newline="")as f:
+    reader=csv.reader(f)
+    file_data=list(reader)
+
+file_data.pop(0)
+
+new_data=[]
+for i in range(len(file_data)):
+    n_numb=file_data[i][1]
+    new_data.append(float(n_numb))
+
+n=len(new_data)
+total=0
+for x in new_data:
+    total+=x
+mean=total/n
+print(mean)
+
+
+
+
+
+
+
+
+
+
